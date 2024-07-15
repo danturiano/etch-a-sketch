99185b80d91bdf0c2d2d4e4a7c6156d1d2b715d2
Daniel <165353614+danturiano@users.noreply.github.com>
2024-07-15 17:52:52 +0800

    added reset btn

--- script.js
+++ script.js
@@ -1,21 +1,60 @@
 const container = document.querySelector(".container");
+const gridBtn = document.querySelector(".gridBtn");
+const resetBtn = document.querySelector(".resetBtn");
 container.style.display = "flex";
 container.style.flexDirection = "column";
 
-// Create the 16x16 grid
-for (let i = 0; i < 16; i++) {
-  const row = document.createElement("div");
-  row.style.display = "flex";
-  row.style.flex = "1";
-  container.appendChild(row);
-  for (let j = 0; j < 16; j++) {
-    const div = document.createElement("div");
-    div.style.color = "red";
-    div.style.flex = "1";
-    div.style.border = "1px solid #ccc"; // Added to visualize the divs
-    row.appendChild(div);
+let newGridSize;
+
+gridBtn.addEventListener("click", () => {
+  newGridSize = prompt("What Grid size do you want?");
+  while (newGridSize > 100) {
+    newGridSize = prompt(
+      "Grid size cannot be greater than 100. What Grid size do you want?"
+    );
+  }
+  while (container.hasChildNodes()) container.removeChild(container.firstChild);
+  setGrid(newGridSize);
+});
+
+resetBtn.addEventListener("click", () => {
+  while (container.hasChildNodes()) container.removeChild(container.firstChild);
+  resetGrid(newGridSize);
+});
+
+function setGrid(gridSize = 16) {
+  for (let i = 0; i < gridSize; i++) {
+    const row = document.createElement("div");
+    row.style.display = "flex";
+    row.style.flex = "1";
+    container.appendChild(row);
+    for (let j = 0; j < gridSize; j++) {
+      const div = document.createElement("div");
+      div.style.color = "red";
+      div.style.flex = "1";
+      div.style.border = "1px solid #ccc";
+      row.appendChild(div);
+    }
   }
 }
+
+function resetGrid(gridSize = 16) {
+  for (let i = 0; i < gridSize; i++) {
+    const row = document.createElement("div");
+    row.style.display = "flex";
+    row.style.flex = "1";
+    container.appendChild(row);
+    for (let j = 0; j < gridSize; j++) {
+      const div = document.createElement("div");
+      div.style.color = "white";
+      div.style.flex = "1";
+      div.style.border = "1px solid #ccc";
+      row.appendChild(div);
+    }
+  }
+}
+
+setGrid();
 
 let isMouseDown = false;
 
